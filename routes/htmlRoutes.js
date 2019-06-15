9521bd88a1e99ce36dfd4df0aeedb04d8953fa9c
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-06-15 13:47:04 -0400

    created the skeleton framework for the files with handlebars and have the index route working

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+// Require all models
+// var db = require("./models");
+
+module.exports = function(app) {
+    // Load index page
+    app.get("/", function(req, res) {
+        res.render("index");
+    });
+
+};
