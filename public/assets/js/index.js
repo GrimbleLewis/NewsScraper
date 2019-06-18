ac61bd31bd40d37828ce9946c9bcf21efce24183
Grimble <grimblelewisthethird@gmail.com>
2019-06-17 21:12:31 -0400

    added the ability to delete an article from your saved articles page, it changes the saved value back to false so it just moves it back to the index page, deleted the saved.js and using index.js for everything

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -29,8 +29,8 @@ $(document).ready(function() {
 
 
 
-    
-$(document).on('click', '.save-article', function (e) {
+// changes the saved value of the article to true and sends to article to the saved.handlebars    
+$(document).on('click', '.save-article', function () {
   var articleId = $(this).data('id');
   $.ajax({
     url: '/save/'+ articleId,
@@ -40,6 +40,22 @@ $(document).on('click', '.save-article', function (e) {
     },
     error: function (error) {
       res.json(error);
+    }
+  });
+});
+
+// deletes selected article from the saved article page
+$(document).on('click', '.delete-article', function (e){
+  e.preventDefault();
+  var id = $(this).data('id');
+  $.ajax({
+    url: '/deletearticle/'+id,
+    type: 'GET',
+    success: function (response) {
+      window.location.href = '/saved';
+    },
+    error: function (error) {
+      showErrorModal(error);
     }
   });
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ var cheerio = require("cheerio");
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/scrape", function(req, res) {
+  app.get("/api/scrape", (req, res) => {
     axios.get("http://www.npr.org/").then(function(response) {
       var $ = cheerio.load(response.data);
 
@@ -33,7 +33,7 @@ module.exports = function(app) {
   });
 
   // Route for getting all Articles from the db
-  app.get("/articles", function(req, res) {
+  app.get("/articles", (req, res) => {
     // Grab every document in the Articles collection
     db.Article.find({})
       .exec()
@@ -41,13 +41,14 @@ module.exports = function(app) {
         // If we were able to successfully find Articles, send them back to the client
         res.json({ Article });
       })
-      .catch(function(err) {
+      .catch((err) => {
         // If an error occurred, send it to the client
         res.json(err);
       });
   });
 
-  app.get("/api/clear", function(req, res) {
+  // clears the current articles from the screen
+  app.get("/api/clear", (req, res) => {
     // Grab every document in the Articles collection
     db.Article.find({})
       .remove()
@@ -55,10 +56,19 @@ module.exports = function(app) {
       .catch(err => res.json(err));
   });
 
+
+  // changes the saved value of the article to true and sends to article to the saved.handlebars
   app.get('/save/:id', (req,res) => {
     db.Article
       .update({_id: req.params.id},{saved: true})
-      .then(result => res.redirect('/saved'))
+      .then(result => res.redirect(result))
+      .catch(err => res.json(err));
+  });
+
+  app.get('/deletearticle/:id', (req,res) =>{
+    db.Article
+    .update({_id: req.params.id},{saved: false})
+      .then(result => res.json(result))
       .catch(err => res.json(err));
   });
 };
